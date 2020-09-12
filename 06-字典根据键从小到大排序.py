2a3ad1d1bc3fbc185969007b19e8e8b953c10072
J_XT <1606778306@qq.com>
2020-09-12 19:35:57 +0800

    Resume

---
+++ 06-字典根据键从小到大排序.py
@@ -0,0 +1,3 @@
+# dict={"name":"zs","age":18,"city":"深圳","tel":"1362626627"}
+
+dict={"name":"zs","age":18,"city":"深圳","tel":"1362626627"}
---
+++ 07-统计字符串每个单词出现的次数.py
@@ -0,0 +1,3 @@
+# 利用collections库的Counter方法统计字符串每个单词出现的次数"kjalfj;ldsjafl;hdsllfdhg;lahfbl;hl;ahlf;h"
+
+
