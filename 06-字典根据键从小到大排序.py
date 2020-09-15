a4119ca98f6d429f52089cc33fa3d281b99adbc2
J_XT <1606778306@qq.com>
2020-09-15 19:13:44 +0800

    Resume

--- 06-字典根据键从小到大排序.py
+++ 06-字典根据键从小到大排序.py
@@ -1,3 +1,9 @@
 # dict={"name":"zs","age":18,"city":"深圳","tel":"1362626627"}
 
-dict={"name":"zs","age":18,"city":"深圳","tel":"1362626627"}
+dict = {"name": "zs", "age": 18,"city": "深圳", "tel": "1362626627"}
+list = sorted(dict.items(),key=lambda i: i[0], reverse=False)
+print("sorted根据字典键排序", list)
+new_dict = {}
+for i in list:
+    new_dict[i[0]] = i[1]
+print('新字典',new_dict)
--- 09-filter方法求出列表所有奇数并构造新列表.py
+++ 09-filter方法求出列表所有奇数并构造新列表.py
@@ -1,3 +1,9 @@
 # filter方法求出列表所有奇数并构造新列表，a =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 a =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def fn(a):
+    return a %2 == 1
+new_list = filter(fn , a)
+new_list = [i for i in new_list]
+print(new_list)
